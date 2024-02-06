07076c22e5650af2405671ebe68145cf2009b58c
Serkan <68036775+SerkanBahtiyar@users.noreply.github.com>
2024-02-07 02:04:20 +0300

    kura

---
+++ randomKura/randomKura/Controllers/DefaultController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using randomKura.Models.Entity;
+namespace randomKura.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+      guncelKuraEntities1 sb=new guncelKuraEntities1();
+        public ActionResult Index()
+        {
+           
+            var random = new Random();
+            var degerler = sb.tablo.ToList();
+            var randomIndex = random.Next(0, degerler.Count);
+            var rastgeleDeger = degerler[randomIndex];
+            return View(new List<tablo> { rastgeleDeger });
+        }
+
+        public PartialViewResult hepsiniListele()
+        {
+            var degerler = sb.tablo.ToList();
+            
+            return PartialView(degerler);
+        }
+        public ActionResult KategoriSil(int id)
+        {
+            var kt = sb.tablo.Find(id);
+            sb.tablo.Remove(kt);
+            sb.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public PartialViewResult ilet()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult ilet(tablo t)
+        {
+            sb.tablo.Add(t);
+            sb.SaveChanges();
+            return PartialView();
+        }
+    }
+}
